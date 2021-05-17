00deb68e764b1a245ff562b6ed563e67edd79a8e
enqidong0207 <83167251+enqidong0207@users.noreply.github.com>
2021-05-17 12:20:23 +0800

    PageEditWorkout add Datepicker

--- HealthHelperMobileApp/HealthHelperMobileApp/PageEditWorkoutLog.xaml.cs
+++ HealthHelperMobileApp/HealthHelperMobileApp/PageEditWorkoutLog.xaml.cs
@@ -94,7 +94,7 @@ namespace HealthHelperMobileApp
             wl.MemberID = App.member.ID;
             wl.WorkoutID = workout.ID;
             wl.WorkoutHours = hours;
-            wl.EditTime = DateTime.Now;
+            wl.EditTime = this.dpEditTime.Date;
             
             if (wlFactory.EditWorkoutLog(wl))
             {
